24ad941f70f242bbd931dd48d25c61c0dbbfea02
suhobae <94097446+Bsuho@users.noreply.github.com>
2022-03-08 15:38:25 +0900

    Add files via upload

--- docs/sub3.js
+++ docs/sub3.js
@@ -44,3 +44,24 @@ $('.menubox-1').mouseenter(function() {
 $('.menubox-1').mouseleave(function() {
   $(this).find(' > ul > li > ul, > .menubox-1__sub-menu-bg').stop().slideUp(00);
 });
+
+
+console.clear();
+
+function SliderBox1__init() {
+  const swiper = new Swiper(".slider-box-1 .swiper-container", {
+    slidesPerView: 1,
+    loop:true,
+    effect: "fade",
+     autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".slider-box-1 .slider-button-next",
+      prevEl: ".slider-box-1 .slider-button-prev",
+    },
+  });
+}
+
+SliderBox1__init();
